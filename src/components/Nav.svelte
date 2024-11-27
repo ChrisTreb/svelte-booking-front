6072d7613b865e6b89df84047dc3ae94c4c87230
CTRL <christophe.trebaol@gmail.com>
2024-11-27 15:23:54 +0100

    Update Nav component

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,34 +1,50 @@
 <!-- Nav -->
-<nav class="navbar fixed-top bg-body-tertiary">
+<nav class="navbar navbar-expand-sm fixed-top bg-body-tertiary">
 	<a class="navbar-brand" href="/">Book App</a>
-	<a class="nav-link active" aria-current="page" href="/">Home</a>
-	<a class="nav-link" href="/places">Places</a>
-	<div class="nav-item dropdown">
-		<a
-			class="nav-link dropdown-toggle"
-			href="/"
-			role="button"
-			data-bs-toggle="dropdown"
-			aria-expanded="false"
-		>
-			Dashboard
-		</a>
-		<ul class="dropdown-menu">
-			<li><a class="dropdown-item" href="/admin/dashboard/place-editor">Place edit</a></li>
-			<li><a class="dropdown-item" href="/admin/dashboard/room-editor">Room edit</a></li>
-			<li><a class="dropdown-item" href="/admin/dashboard/users">Users</a></li>
-		</ul>
+	<button
+		class="navbar-toggler"
+		type="button"
+		data-bs-toggle="collapse"
+		data-bs-target="#collapsibleNavbar"
+	>
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="collapsibleNavbar">
+		<a class="nav-link active" aria-current="page" href="/">Home</a>
+		<a class="nav-link" href="/places">Places</a>
+		<div class="nav-item dropdown">
+			<a
+				class="nav-link dropdown-toggle"
+				href="/"
+				role="button"
+				data-bs-toggle="dropdown"
+				aria-expanded="false"
+			>
+				Dashboard
+			</a>
+			<ul class="dropdown-menu">
+				<li><a class="dropdown-item" href="/admin/dashboard/place-editor">Place edit</a></li>
+				<li><a class="dropdown-item" href="/admin/dashboard/room-editor">Room edit</a></li>
+				<li><a class="dropdown-item" href="/admin/dashboard/users">Users</a></li>
+			</ul>
+		</div>
 	</div>
 </nav>
 
 <style>
-	.navbar {
-		justify-content: left;
+	.navbar-brand {
+		margin-left: 90px;
+		font-weight: bolder;
 	}
 
-	.navbar-brand {
-		margin-left: 50px;
-		font-weight: bolder;
+	.navbar-toggler {
+		position: absolute;
+		top: 10px;
+		left: 20px;
+	}
+
+	#collapsibleNavbar {
+		margin: 10px 0;
 	}
 
 	.nav-link {
