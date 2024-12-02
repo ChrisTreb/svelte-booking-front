8d97edfe661b957592ca9ee85b7871aae41bd150
CTRL <christophe.trebaol@gmail.com>
2024-12-02 15:14:08 +0100

    Signup page

--- src/routes/sign-up/+page.svelte
+++ src/routes/sign-up/+page.svelte
@@ -6,6 +6,10 @@
     let user: AppUser;
     user = new AppUser(0, new Date(), "", "", "", "", new Date(), "", "", "", "", "", "", "");
 
+    function logUser() {
+        console.log(user);
+    }
+
 </script>
 
 <Nav />
@@ -17,13 +21,13 @@
         <h1>Sign up form</h1>
         <div class="form-row">
             <div class="mb-3">
-                <label for="login" class="form-label">Login</label>
+                <label for="login" class="form-label">Login <span class="required">*</span></label>
                 <input required type="text" class="form-control" id="login" minlength="1" bind:value={user.login}/>
             </div>
 
             <div class="mb-3">
-                <label for="gender" class="form-label">Gender</label>
-                <select required class="form-select" aria-label="Default select example">
+                <label for="gender" class="form-label">Gender <span class="required">*</span></label>
+                <select required class="form-select" bind:value={user.gender}>
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
                     <option value="Non-binary">Non-binary</option>
@@ -40,57 +44,58 @@
 		
         <div class="form-row">
             <div class="mb-3">
-                <label for="firstname" class="form-label">Firstname</label>
-                <input required type="text" class="form-control" id="firstname" minlength="1" />
+                <label for="firstname" class="form-label">Firstname <span class="required">*</span></label>
+                <input required type="text" class="form-control" id="firstname" minlength="1" bind:value={user.firstname} />
             </div>
     
             <div class="mb-3">
-                <label for="lastname" class="form-label">Lastname</label>
-                <input required type="text" class="form-control" id="lastname" minlength="1" />
+                <label for="lastname" class="form-label">Lastname <span class="required">*</span></label>
+                <input required type="text" class="form-control" id="lastname" minlength="1" bind:value={user.lastname} />
             </div>
 
             <div class="mb-3">
-                <label for="birth-date" class="form-label">Birth date</label>
-                <input required type="date" class="form-control" id="birth-date" value="1970-01-01" />
+                <label for="birth-date" class="form-label">Birth date <span class="required">*</span></label>
+                <input required type="date" class="form-control" id="birth-date" bind:value={user.date_of_birth} />
             </div>
         </div>
 		
 
         <div class="form-row">
             <div class="mb-3">
-                <label for="country" class="form-label">Country</label>
-                <input required type="text" class="form-control" id="country" minlength="1" />
+                <label for="country" class="form-label">Country <span class="required">*</span></label>
+                <input required type="text" class="form-control" id="country" minlength="1" bind:value={user.country} />
             </div>
     
             <div class="mb-3">
-                <label for="city" class="form-label">City</label>
-                <input required type="text" class="form-control" id="city" minlength="1" />
+                <label for="city" class="form-label">City <span class="required">*</span></label>
+                <input required type="text" class="form-control" id="city" minlength="1" bind:value={user.city} />
             </div>
         </div>
 		
         <div class="mb-3 long-input">
-            <label for="address" class="form-label">Address</label>
-            <input required type="text" class="form-control" id="address" minlength="1" />
+            <label for="address" class="form-label">Address <span class="required">*</span></label>
+            <input required type="text" class="form-control" id="address" minlength="1" bind:value={user.address} />
         </div>
 		
         <div class="form-row">
             <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input required type="email" class="form-control" id="email" minlength="10" />
+                <label for="email" class="form-label">Email <span class="required">*</span></label>
+                <input required type="email" class="form-control" id="email" minlength="10" bind:value={user.email} />
             </div>
     
             <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" minlength="8" required />
+                <label for="password" class="form-label">Password <span class="required">*</span></label>
+                <input required type="password" class="form-control" id="password" aria-describedby="passwordHelp" minlength="8" bind:value={user.password} />
+                <div id="passwordHelp" class="form-text">For security purpose : 8 characters minimum</div>
             </div>
         </div>
 
         <div class="mb-3 long-input">
             <label for="avatar" class="form-label">Avatar</label>
-            <input type="password" class="form-control" id="avatar" minlength="10" required />
+            <input type="text" class="form-control" id="avatar" minlength="10" bind:value={user.image} />
         </div>
 
-		<button type="submit" class="btn btn-primary">Register</button>
+		<button type="submit" on:click={logUser} class="btn btn-primary">Register</button>
 	</form>
 </div>
 
@@ -125,5 +130,9 @@
     .btn {
         margin-top: 20px;
         margin-left: 50px;
+    }
+
+    .required {
+        color: red;
     }
 </style>
