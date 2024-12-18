be07ae9ae60edca77fc2723d52b2fe36b6145e03
CTRL <christophe.trebaol@gmail.com>
2024-12-18 19:24:08 +0100

    Btn top + map

--- src/components/BtnPageTop.svelte
+++ src/components/BtnPageTop.svelte
@@ -2,44 +2,40 @@
 
 	let btn: HTMLElement ;
 
-	window.onscroll = function () {
-		scrollFunction();
-	};
-
-	function scrollFunction() {
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			btn.style.display = 'block';
-		} else {
-			btn.style.display = 'none';
-		}
-	}
-
 	function backToTop() {
 		document.body.scrollTop = 0;
 		document.documentElement.scrollTop = 0;
 	}
 </script>
 
-<button
-	type="button"
-	class="btn btn-floating btn-lg"
-	id="btn-back-to-top"
-    bind:this={btn}
-	on:click={backToTop}
->
-	<i class="fas fa-arrow-up"></i>
-</button>
+{#if document.readyState == "complete" }
+	<button
+		type="button"
+		class="btn btn-floating btn-lg"
+		id="btn-back-to-top"
+		bind:this={btn}
+		on:click={backToTop}
+	>
+		<i class="fas fa-arrow-up"></i>
+	</button>
+{/if}
 
 <style>
 	#btn-back-to-top {
+		display: none;
         z-index: 150;
 		position: fixed;
 		bottom: 20px;
 		right: 20px;
-		display: none;
         border-radius: 25px;
         background-color: rgb(240, 223, 198);
         box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
         border: 1px solid black;
+	}
+
+	@media (max-width: 768px) {
+		#btn-back-to-top {
+			display: block;
+		}
 	}
 </style>
--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -3,6 +3,7 @@
 	import { getPlaceCoordinates } from '../dao/placeDao';
 
     let map: L.Map;
+	
     export let placeId: number;
 
     async function initMap() {
@@ -10,14 +11,15 @@
         let data: any = await getPlaceCoordinates(placeId);
 
 		if (typeof window !== 'undefined') {
-			map = L.map("map").setView([ data.lat , data.lng], 12);
+			map = L.map("map").setView([ data.lat , data.lng], 13);
 			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 				attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
 				subdomains: ['a', 'b', 'c']
 			}).addTo(map);
+
+			// Add marker
+			L.marker([ data.lat , data.lng]).addTo(map);
 		}
 	}
-    
     initMap();
-    
 </script>
